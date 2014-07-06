905e06046a40ae98e1337e6d471c8b8573daf4cf
Dillon Chalupsky <slammajamma28@gmail.com>
2014-07-06 16:20:02 -0400

    Begin UI

---
+++ PSTHallOfFame/src/com/slamma/jamma/HallOfFameTopPanel.java
@@ -0,0 +1,18 @@
+package com.slamma.jamma;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class HallOfFameTopPanel extends JPanel {
+	private ImageIcon pstLogo = null;
+	private String hallOfFame = "Hall of Fame";
+	private String baseballString = "baseball";
+	private String basketballString = "basketball";
+	private String footballString = "football";
+	private String hockeyString = "hockey";
+	private String mmaString = "mma";
+	private String otherString = "other";
+	private String racingString = "racing";
+	private String soccerString = "soccer";
+	private String wrestlingString = "wrestling";
+}
